6e6410e0da6e1d237279856bae5b146d2a72590b
giovanna432 <giovanna.santos2@alunos.unis.edu.br>
2025-04-14 21:40:09 -0300

    Update sumula.java

--- src/br/edu/unis/ads/campeonatobrasileiro/sumula.java
+++ src/br/edu/unis/ads/campeonatobrasileiro/sumula.java
@@ -2,7 +2,27 @@ class Sumula{
     int cartao;
     String acontecimentos;
 
-    Sumula(int, String args[]){
-
+   public Sumula(int cartao, String acontecimentos) {
+        this.cartao = cartao;
+        this.acontecimentos = acontecimentos;
+       
+  public int getCartao() {
+        return cartao;
+    }
+  public void setCartao(int cartao) {
+        this.cartao = cartao;
+    }
+   public String getAcontecimentos() {
+        return acontecimentos;
+    }
+  public void setAcontecimentos(String acontecimentos) {
+        this.acontecimentos = acontecimentos;
+    }
+  public String toString() {
+        return "Cartões: " + cartao + "\nAcontecimentos: " + acontecimentos;
+    }
+    
+    public void adicionarAcontecimento(String novoAcontecimento) {
+        this.acontecimentos += "\n- " + novoAcontecimento;
     }   
 }
